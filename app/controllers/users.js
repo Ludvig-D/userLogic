e9633a93d9eb9aa730df1b95c8582ae889fa6a43
Ludvig-D <74721111+Ludvig-D@users.noreply.github.com>
2025-09-15 10:43:21 +0200

    Fixed messages are sent

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -5,11 +5,11 @@ module.exports.signupUser = async (req, res) => {
     const { username, email, password } = req.body;
     const usernameInUse = await User.findOne({ username: username });
     if (usernameInUse) {
-      res.status(400).json({ message: 'Username already exists' });
+      res.status(400).message('Username already exists');
     }
     const emailInUse = await User.findOne({ email: email });
     if (emailInUse) {
-      res.status(400).json({ message: 'Email already in use' });
+      res.status(400).message('Email already in use');
     }
 
     const user = new User({
@@ -18,7 +18,7 @@ module.exports.signupUser = async (req, res) => {
       password: password,
     });
     await user.save();
-    res.status(200).json({ message: 'User created successfully' });
+    res.status(200).message('User created successfully');
   } catch (error) {
     console.error(error);
 
@@ -31,16 +31,16 @@ module.exports.loginUser = async (req, res) => {
     const { username, password } = req.body;
     const userExists = await User.findOne({ username: username });
     if (!userExists) {
-      res.status(400).json({ message: 'Username or password is incorrect' });
+      res.status(400).message('Username or password is incorrect');
     }
 
     const validPassword = await userExists.verifyPassword(password);
 
     if (!validPassword) {
-      res.status(400).json({ message: 'Username or password is incorrect' });
+      res.status(400).message('Username or password is incorrect');
     }
 
-    res.status(200).json({ message: 'Login succesfull' });
+    res.status(200).message('Login succesfull');
   } catch (error) {
     console.error(error);
     res.status(500).message('Something went wrong on server side');
